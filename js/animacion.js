0407983ad6581756996d7c7f5155fd4833c1a925
sergiioss <sergiiptx@gmail.com>
2023-11-22 01:46:46 +0100

    animaciones bolas rebotando

--- js/animacion.js
+++ js/animacion.js
@@ -6,7 +6,12 @@ const game = {
     bolaX:50,
     bolaY:100,
     colorBola:"red",
-    disX:30
+    dx:15,
+    dy:-15,
+    limiteDerecha:0,
+    limiteIzquierda:0,
+    limiteAbajo:0,
+    limiteArriba:0
 };
 
 
@@ -19,8 +24,7 @@ window.onload= function(){
 
         if(game.ctx){
             //styles
-           game.ctx.lineWidth = game.radio;
-           game.ctx.fillStyle= game.colorBola;
+           rebote();
            mueve();
            setInterval(mueve,60);
 
@@ -33,9 +37,49 @@ window.onload= function(){
     }
 }
 
+const rebote = () =>{
+    game.ctx.lineWidth = game.radio;
+    game.ctx.fillStyle= game.colorBola;
+
+    game.limiteDerecha= game.canvas.width - game.radio;
+    game.limiteIzquierda= game.radio;
+
+    game.limiteAbajo=game.canvas.height -game.radio;
+    game.limiteArriba=game.radio
+
+}
+
 const mueve = () =>{
-    game.bolaX += game.disX
+    game.ctx.clearRect(0,0, game.canvas.width, game.canvas.height);
+    verificar();
+    game.bolaX += game.dx;
+    game.bolaY += game.dy;
     game.ctx.beginPath();
     game.ctx.arc(game.bolaX, game.bolaY, game.radio, 0,2*Math.PI,true);
     game.ctx.fill();
+}
+
+const verificar = () =>{
+    let nbolaX = game.bolaX + game.dx;
+    let nbolaY = game.bolaY+ game.dy;
+
+    if(nbolaX > game.limiteDerecha){
+        game.dx *= -1;
+        nbolaX = game.limiteDerecha;
+    }
+    if(nbolaX < game.limiteIzquierda){
+        game.dx *= -1;
+        nbolaX = game.limiteIzquierda;
+    }
+    if(nbolaY > game.limiteAbajo){
+        game.dy *= -1;
+        nbolaY = game.limiteAbajo;
+    }
+    if(nbolaY < game.limiteArriba){
+        game.dy *= -1;
+        nbolaY = game.limiteArriba;
+    }
+
+    game.bolaX = nbolaX;
+    game.bolaY = nbolaY;
 }
